875df6076ea3878b819004d7ccfbc2adf6a0f833
oliveoil212 <xxx212212@gapp.nthu.edu.tw>
2021-03-26 09:49:43 +0800

    then buttom ok

--- main.cpp
+++ main.cpp
@@ -2,34 +2,48 @@
 #include "uLCD_4DGL.h"
 #define S 8
 using namespace std::chrono;
-DigitalIn buttom_up(D13);
-DigitalIn buttom_down(D12);
-DigitalIn buttom_select(D11);
+InterruptIn buttom_up(D13);
+InterruptIn buttom_down(D12);
+InterruptIn buttom_select(D11);
 uLCD_4DGL uLCD(D1, D0, D2); // serial tx, serial rx, reset pin;
 AnalogOut Aout(PA_4); // D7 pin
 
 Ticker counter;
 int cnt = 0;
 double period;
-void counting () {
+int freq = 100;
+void count_func() {
     cnt++;
 }
-
+void freq_up(){
+    freq++;
+}
+void freq_down(){
+    freq--;
+}
 int main() {
-    // uLCD.background_color(0);
-    // uLCD.cls();
-    // uLCD.printf("108061213_HW2\n");
-    int freq = 100;
+    uLCD.reset();
+    uLCD.background_color(0);
+    uLCD.cls();
+    uLCD.printf("108061213_HW2\n");
+    buttom_down.rise(&freq_down);
+    buttom_up.rise(&freq_up);
+    while(buttom_select != 1) {
+        uLCD.locate(2,2);
+        uLCD.printf("%4d",freq);
+        // printf("%d\r\n", freq);
+    }
     printf("start\r\n");
-    // period = 1000000.0 / freq;
-    period = 100;
+    period = 1000000.0 / freq;
+    // period = 10000;
     double increasing_time = period * S /10;
     double decreasing_time =period - increasing_time;
     double out;
     // std::chrono::microseconds delay(1);
-    // counter.attach_us(&counting,int (delay.count()));
+    // counter.attach_us(&count_func,int (delay.count()));
+    // us_timestamp_t delay = 100;
+    // counter.attach_us(&count_func, delay us);
     while(1) {
-        printf("%f   %f", increasing_time, decreasing_time);
         if(cnt >= period) cnt = 0;
         if(cnt >= increasing_time) {
             out = 1 -  (cnt - increasing_time) / decreasing_time;
@@ -38,15 +52,10 @@ int main() {
             out = cnt / increasing_time;
         }
         Aout = out;
+        printf("%f", 3.3*out);
         wait_us(1);
         cnt++;
-        printf("cnt=%d out = %f de= %f in= %f\n", cnt, out, decreasing_time, increasing_time);
+        // printf("cnt=%d out = %f de= %f in= %f\n", cnt, out, decreasing_time, increasing_time);
     }
-    // while(buttom_select != 1) {
-    //     if(buttom_up) freq+=1;
-    //     if(buttom_down) freq-=1;
-    //     uLCD.locate(3,2);
-    //     uLCD.printf("%d",freq);
-    // }
 }
 
