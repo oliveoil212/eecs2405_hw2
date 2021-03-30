95b511d8c777b124806c037a7a6cd61361d88e69
oliveoil212 <55798323+oliveoil212@users.noreply.github.com>
2021-03-30 22:54:25 +0800

    Update main.cpp
    ok

--- main.cpp
+++ main.cpp
@@ -7,55 +7,116 @@ InterruptIn buttom_down(D12);
 InterruptIn buttom_select(D11);
 uLCD_4DGL uLCD(D1, D0, D2); // serial tx, serial rx, reset pin;
 AnalogOut Aout(PA_4); // D7 pin
-
+AnalogIn Ain(A0);
+Thread g, s;
 Ticker counter;
 int cnt = 0;
-double period;
-int freq = 100;
+long int  period;
+int freq = 1;
+int selected = 0;
+int selection =2;
+bool sampling = 0;
+float out;
 void count_func() {
     cnt++;
 }
 void freq_up(){
-    freq++;
+    selection++;
+    if(selection > 2) selection = 1;
 }
 void freq_down(){
-    freq--;
+    selection--;
+    if(selection < 1) selection = 2;
+}
+void select_isr(){
+    selected = 1;
+}
+void generator_thread() {
+
+    float i;
+    while(1) {
+        if (1) {
+            if (selection == 1) {        // 200Hz
+                for (i = 0.0f; i < 1; i+=0.0075635f) {
+                    Aout = i;
+                }
+                for (i = 1.0f; i > 0.0f; i -= 0.001896f) {
+                    Aout = i;
+                }
+            }
+            else if (selection == 2) {        // 20Hz
+                for (i = 0.0f; i < 1; i+=0.00074635f) {
+                    Aout = i;
+                }
+                for (i = 1.0f; i > 0.0f; i -= 0.0001776f) {
+                    Aout = i;
+                }
+            }    
+        }
+    }
+}
+int sample = 500;
+float ADCdata[500];
+void sample_thread() {
+    int i = 0;
+    sampling = 1;
+    // t.start();
+    for (i = 0; i < sample; i++){
+        // Aout = Ain;
+        ADCdata[i] = Ain;
+        ThisThread::sleep_for(1000ms/sample); // sampling rate = 500/s 實際55/s
+    }
+    sampling = 0;
+    //t.stop();
+    //auto ms = chrono::duration_cast<chrono::milliseconds>(t.elapsed_time()).count();
+    for (i = 0; i < sample; i++) {
+        printf("%f\r\n", ADCdata[i]);
+    }
+    //printf ("Timer time: %llu ms\n", ms);
 }
 int main() {
     uLCD.reset();
     uLCD.background_color(0);
     uLCD.cls();
     uLCD.printf("108061213_HW2\n");
+    uLCD.printf("cutoff freq 152Hz\n");
+    uLCD.text_width(2); //3X size text
+    uLCD.text_height(2);
+    uLCD.color(GREEN);
+    uLCD.locate(1,2);
+    uLCD.printf("200Hz");
+    uLCD.locate(1,4);
+    uLCD.printf("20Hz");
     buttom_down.rise(&freq_down);
     buttom_up.rise(&freq_up);
-    while(buttom_select != 1) {
-        uLCD.locate(2,2);
-        uLCD.printf("%4d",freq);
-        // printf("%d\r\n", freq);
+    buttom_select.rise(&select_isr);
+    g.start(generator_thread);
+    while(selected != 1) {
+        //  uLCD.locate(0,3);
+        //         uLCD.printf("%d",selection);
+       switch(selection){
+           case 1: 
+                // uLCD.textbackground_color(WHITE);
+                uLCD.locate(0,2);
+                uLCD.printf("-");
+                uLCD.locate(0,4);
+                uLCD.printf(" ");
+                break;
+            case 2: 
+                // uLCD.textbackground_color(WHITE);
+                uLCD.locate(0,2);
+                uLCD.printf(" ");
+                uLCD.locate(0,4);
+                uLCD.printf("-");
+                break;
+                // uLCD.printf("error");
+       }
     }
-    printf("start\r\n");
-    period = 1000000.0 / freq;
-    // period = 10000;
-    double increasing_time = period * S /10;
-    double decreasing_time =period - increasing_time;
-    double out;
-    // std::chrono::microseconds delay(1);
-    // counter.attach_us(&count_func,int (delay.count()));
-    // us_timestamp_t delay = 100;
-    // counter.attach_us(&count_func, delay us);
-    while(1) {
-        if(cnt >= period) cnt = 0;
-        if(cnt >= increasing_time) {
-            out = 1 -  (cnt - increasing_time) / decreasing_time;
-        }
-        else {
-            out = cnt / increasing_time;
-        }
-        Aout = out;
-        printf("%f", 3.3*out);
-        wait_us(1);
-        cnt++;
-        // printf("cnt=%d out = %f de= %f in= %f\n", cnt, out, decreasing_time, increasing_time);
-    }
-}
+    uLCD.locate(0,6);
+    uLCD.printf(" start!");
 
+    sample_thread();
+    uLCD.locate(0,6);
+    uLCD.printf(" done!");
+    
+}
